10c6f6fab83506da047c126fdc01f52f1e861ebb
ivlevks <ivlevks@yandex.ru>
2022-01-11 20:45:48 +0300

    added forget files

---
+++ src/main/java/com/ivlevks/firstAppOnSpringBoot/FirstAppOnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ivlevks.firstAppOnSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppOnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppOnSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivlevks/firstAppOnSpringBoot/FirstAppOnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivlevks.firstAppOnSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppOnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
