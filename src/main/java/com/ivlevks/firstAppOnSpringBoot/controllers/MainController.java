6b14b58342b27f904140600ef96a9724123b43ea
ivlevks <ivlevks@yandex.ru>
2022-01-12 21:25:59 +0300

    front page

--- src/main/java/com/ivlevks/firstAppOnSpringBoot/controllers/MainController.java
+++ src/main/java/com/ivlevks/firstAppOnSpringBoot/controllers/MainController.java
@@ -1,18 +1,16 @@
 package com.ivlevks.firstAppOnSpringBoot.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
     }
 
 }
