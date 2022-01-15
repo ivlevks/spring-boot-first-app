5e0f55d2522976ad735c090dfa775a2c87a543ca
ivlevks <ivlevks@yandex.ru>
2022-01-15 20:47:23 +0300

    saving new article

--- src/main/java/com/ivlevks/firstAppOnSpringBoot/controllers/BlogController.java
+++ src/main/java/com/ivlevks/firstAppOnSpringBoot/controllers/BlogController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -13,11 +15,25 @@ public class BlogController {
     @Autowired
     private PostRepository postRepository;
 
-
     @GetMapping("/blog")
     public String blogMain(Model model) {
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
         return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,
+                              @RequestParam String anons,
+                              @RequestParam String full_text,
+                              Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
